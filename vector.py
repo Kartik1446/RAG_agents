fdb4de2c38d0fd94709b0dec94e62db9da4cfcdc
Kartik <kartik140406@gmail.com>
2026-06-24 22:29:20 +0530

    complete

---
+++ vector.py
@@ -0,0 +1,36 @@
+# Vector Search is a database; it's gonna be hosted locally on my own computer using ChromaDB.
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+import os
+import pandas as pd
+
+df= pd.read_csv("data/realistic_restaurant_reviews.csv")
+embeddings= OllamaEmbeddings(model="mxbai-embed-large")
+
+db_location="./chroma_langchain_db"
+add_documents= not os.path.exists(db_location)
+
+
+vector_store= Chroma(collection_name="restaurant_reviews",
+                                    persist_directory=db_location,
+                                    embedding_function=embeddings)
+
+if add_documents:
+    documents=[]
+    ids= []
+
+    for i, row in df.iterrows():
+        document= Document(page_content=row["Title"] + " " + row["Review"], 
+                           metadata={"rating": row["Rating"], "date": row["Date"]},
+                           id=str(i)
+                           )
+        documents.append(document)
+        ids.append(str(i))
+
+    vector_store.add_documents(documents=documents, ids=ids)
+    vector_store.persist()
+
+retriever= vector_store.as_retriever(
+    search_kwargs={"k": 5} # keyword argument to specify the number of results to return
+)   
